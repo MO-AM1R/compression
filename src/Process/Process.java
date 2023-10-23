83f4d77a626a0084d11a036de026c82aba2d4943
Mohamed Amir <me.mohamedamir@gmail.com>
2023-10-23 09:34:29 +0300

    Add Process enum and it's cases

---
+++ src/Process/Process.java
@@ -0,0 +1,17 @@
+package Process;
+
+/**
+ * <pre>
+ *    This enum it handle the case will be made on the file
+ *     {@code COMPRESS}: To compress file
+ *     {@code DECOMPRESS}: To decompress file
+ * </pre>
+ * <blockquote>
+ * @author <strong style="color: 'white'">Mohamed Amir</strong>
+ * @version <strong style="color: 'white'">1.0</strong>
+ * </blockquote>
+ */
+public enum Process {
+    COMPRESS,
+    DECOMPRESS
+}
