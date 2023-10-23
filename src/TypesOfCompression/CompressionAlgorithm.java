f0dfbb9593b4fd90b6d65932ff5db245dd8d1369
Mohamed Amir <me.mohamedamir@gmail.com>
2023-10-23 09:49:54 +0300

    Add CompressionAlgorithm interface and it's methods

---
+++ src/TypesOfCompression/CompressionAlgorithm.java
@@ -0,0 +1,45 @@
+package TypesOfCompression;
+
+/**
+ * <pre>
+ *    This interface represent the methods of each Compression Algorithm
+ *    it has two methods
+ *         {@code compress}
+ *         {@code decompress}
+ * </pre>
+ *
+ * <blockquote>
+ * @version <strong style="color: 'white'">1.0.0</strong>
+ * @author <strong style="color: 'white'">Mohamed Amir</strong>
+ * </blockquote>
+ */
+public interface CompressionAlgorithm {
+
+    /**
+     * <pre>
+     *This method {@code compress} it compress the
+     *content of {@code content} and return the tags
+     *as {@code String}
+     * </pre>
+     *
+     * <blockquote>
+     * @version <strong style="color: 'white'">1.0.0</strong>
+     * @author <strong style="color: 'white'">Mohamed Amir</strong>
+     * </blockquote>
+     */
+    String compress(String content) ;
+
+    /**
+     * <pre>
+     *This method {@code deCompress} it decompress the
+     *content of {@code content} and return the text
+     *as {@code String}
+     * </pre>
+     *
+     * <blockquote>
+     * @version <strong style="color: 'white'">1.0.0</strong>
+     * @author <strong style="color: 'white'">Mohamed Amir</strong>
+     * </blockquote>
+     */
+    String deCompress(String content) ;
+}
