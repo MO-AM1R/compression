ecd814c9f33370f3c52579fd34a903732cbba443
Mohamed Amir <me.mohamedamir@gmail.com>
2023-10-24 11:45:44 +0300

    Refactor run branch and merge with typesOfCompressions

---
+++ src/Run/Run.java
@@ -0,0 +1,21 @@
+package Run;
+import Process.Process ;
+
+/**
+ * <pre>
+ * This class will handle the run of program
+ *    - will compress files or decompress
+ *    - will handle output and input files
+ * </pre>
+ * <blockquote>
+ *  @author <strong style="color: 'white'">Mohamed Amir</strong>
+ *  @version <strong style="color: 'white'">1.0</strong>
+ *  </blockquote>
+ */
+public class Run {
+    Process process ;
+    public Run(Process p){
+
+    }
+    public void run(){}
+}
