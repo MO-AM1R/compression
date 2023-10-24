b7ce490b6aa968a474db5531b7c78d9b262fede5
Mohamed Amir <me.mohamedamir@gmail.com>
2023-10-24 12:21:28 +0300

    finish process

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,5 @@
-import Process.Process ;
-import Process.Decompress ;
-import TypesOfCompression.CompressionAlgorithm;
-import TypesOfCompression.LZ77;
-
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Process process = new Decompress() ;
-        process.runProcess(new LZ77()) ;
+    public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 }
