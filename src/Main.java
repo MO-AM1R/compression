d7e731158c9aef749515dfdbf1fac27acc1f641f
Mohamed Amir <me.mohamed.amir@gmail.com>
2023-10-26 18:14:34 +0300

    Push sample of run in main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import Run.Run;
+import TypesOfCompression.CompressionAlgorithm;
+import TypesOfCompression.LZ77;
+import Process.Process;
+import Process.Compress;
+import Process.Decompress;
+import TypesOfCompression.LZ78;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CompressionAlgorithm compressionAlgorithm = new LZ78() ;
+        Process process = new Decompress() ;
+        Run run = new Run(process, compressionAlgorithm);
+        run.run();
     }
 }
