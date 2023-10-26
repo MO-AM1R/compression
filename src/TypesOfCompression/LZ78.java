5ec7e1ee4bcfb69e58e19d55573dd67b54999d44
Mohamed Amir <me.mohamed.amir@gmail.com>
2023-10-26 17:40:35 +0300

    finish compress method of LZ78

--- src/TypesOfCompression/LZ78.java
+++ src/TypesOfCompression/LZ78.java
@@ -1,6 +1,8 @@
 package TypesOfCompression;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *<pre>
@@ -30,7 +32,37 @@ public class LZ78 implements CompressionAlgorithm{
      */
     @Override
     public String compress(String content) {
-        return null;
+        StringBuilder tags = new StringBuilder() ;
+        Map<String, Integer> dictionary = new HashMap<>();
+        int index = 0, counter = 1;
+
+        while (index < content.length()){
+            if (!dictionary.containsKey(content.charAt(index) + "")){
+                dictionary.put(content.charAt(index) + "", counter++) ;
+                tags.append("<0, ").append(content.charAt(index++)).append(">\n");
+                continue ;
+            }
+
+            StringBuilder currentPattern = new StringBuilder();
+            currentPattern.append(content.charAt(index++)) ;
+            while (index < content.length() &&
+                    dictionary.containsKey(currentPattern.toString() + content.charAt(index))){
+                currentPattern.append(content.charAt(index++)) ;
+            }
+            if (index < content.length()) {
+                tags.append("<").append(dictionary.get(currentPattern.toString())).append(", ")
+                        .append(content.charAt(index)).append(">\n");
+
+                currentPattern.append(content.charAt(index++)) ;
+                dictionary.put(currentPattern.toString(), counter++) ;
+            }
+            else{
+                tags.append("<").append(dictionary.get(currentPattern.toString())).append(", ")
+                        .append("null").append(">");
+            }
+        }
+
+        return tags.toString() ;
     }
 
     /**
